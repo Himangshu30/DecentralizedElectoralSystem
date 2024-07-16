aebe4eb48c9b8db20c62a7fab07583bc15b13281
Bikash Dutta <bikash27dutta@gmail.com>
2024-07-17 02:45:43 +0530

    Solidity files are updated

--- contracts/Voter.sol
+++ contracts/Voter.sol
@@ -11,11 +11,38 @@ contract Voter {
         es = ES(esAddress);
     }
 
-    function getVoterDetails(string memory _uid) public view
-    returns (string memory, string memory, uint, uint, uint, string memory, string memory, int, int, bool) {
+    function fetchVoterDetails(string memory _uid) public view
+    returns (string memory, string memory, uint, uint, uint, string memory, string memory, int, int, bool, string[] memory) {
         ES.Voter memory voter = es.getVoterDetails(_uid);
         return (voter.name, voter.gender, voter.dob.day, voter.dob.month, voter.dob.year, voter.constituency,
-        voter.phoneNumber, voter.residentalAddress.latitude, voter.residentalAddress.longitude, voter.isVoted);
+        voter.phoneNumber, voter.residentalAddress.latitude, voter.residentalAddress.longitude, voter.isVoted,
+        fetchCandidates(voter.constituency));
+    }
+
+    function fetchCandidates(string memory _constituency) private view returns (string[] memory){
+        ES.Candidate[] memory candidates = es.getCandidates(_constituency);
+        string[] memory list = new string[](candidates.length);
+
+        for (uint i = 0; i < candidates.length; i++) {
+            string memory data = string(abi.encodePacked(candidates[i].name, "(", candidates[i].politicalAffiliation, ")"));
+            list[i] = data;
+        }
+
+        return list;
+    }
+
+    function vote(
+        string memory _voter_uid,
+        string memory _constituency,
+        uint256 _index
+    ) public returns (string memory) {
+        (string memory _msg, bool isEligible) = es.checkVoterValidity(_voter_uid);
+        if(isEligible) {
+        es.vote(_voter_uid, _constituency,  _index);
+        return "Voted successfully";
+        } else {
+            return _msg;
+        }
     }
 
 }
